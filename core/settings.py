0a2b40847f1629b0015ebb9569206ec6ced66fec
Inzamul Hoque <inzamol@gmail.com>
2022-03-04 13:33:09 +0530

    Email Feature updated

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "Your email"
-EMAIL_HOST_PASSWORD = "Your email password"
+EMAIL_HOST_USER = "testemail98761a@gmail.com"
+EMAIL_HOST_PASSWORD = "INZAMOL."
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
