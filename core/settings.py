cb1c0f547d06082c0b38c17c45da689d685816fd
Inzamul Hoque <inzamol@gmail.com>
2022-03-04 13:33:39 +0530

    Email Feature updated

--- core/settings.py
+++ core/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "testemail98761a@gmail.com"
-EMAIL_HOST_PASSWORD = "INZAMOL."
+EMAIL_HOST_PASSWORD = ""
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
